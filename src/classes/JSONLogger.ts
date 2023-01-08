2b8e97cde418f2709cfda7296886a6d298f0c087
Kosmas Papadatos <kosmas.papadatos@gmail.com>
2023-01-08 21:40:16 +0200

    Fix

--- src/classes/JSONLogger.ts
+++ src/classes/JSONLogger.ts
@@ -2,16 +2,30 @@ import { jsonStringifySafe } from '../fn/jsonStringifySafe';
 import { Logger } from './Logger';
 
 export class JSONLogger extends Logger {
-    public constructor(private options: IJSONLoggerOptions) { super(); }
+    private readonly defaultOptions: IJSONLoggerOptions = {
+        useDefaultFields: true
+    };
+    private readonly options: IJSONLoggerOptions;
+    public constructor(options?: Partial<IJSONLoggerOptions>) {
+        super();
+        this.options = {
+            ...this.defaultOptions,
+            ...options
+        };
+    }
     public log(input: any) { console.log(this.stringify(input)); }
     public warn(input: any) { console.warn(this.stringify(input)); }
     public debug(input: any) { console.debug(this.stringify(input)); }
     public error(input: any) { console.error(this.stringify(input)); }
     public info(input: any) { console.info(this.stringify(input)); }
     private stringify(input: any) {
+        if (typeof input === 'string') {
+            input = { msg: input };
+        }
+
         if (this.options.useDefaultFields) {
             input = {
-                message: input.message,
+                msg: input.message,
                 ts: new Date(),
                 stack: input.stack,
                 ...input
@@ -23,5 +37,5 @@ export class JSONLogger extends Logger {
 }
 
 export interface IJSONLoggerOptions {
-    useDefaultFields: boolean;
+    useDefaultFields?: boolean;
 }
