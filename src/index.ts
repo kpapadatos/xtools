93c6ac42a0c026c841d1e729fead08de15b05d06
Kosmas Papadatos <kosmas.papadatos@gmail.com>
2023-01-08 21:27:56 +0200

    Fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
+export * from './classes/ConsoleLogger';
+export * from './classes/JSONLogger';
+export * from './classes/Logger';
+export * from './classes/NoopLogger';
 export * from './fn/AsyncReturnType';
 export * from './fn/concurrently';
 export * from './fn/defer';
+export * from './fn/jsonStringifySafe';
 export * from './fn/sleep';
 export * from './fn/withRetries';
 export * from './fn/withTimeout';
+
 
